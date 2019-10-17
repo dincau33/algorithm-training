df587ccc4dab5e85dddfbea104d1d840d2eac752
dincau33 <dincau33@gmail.com>
2019-10-17 14:28:51 -0700

    Fibonacci number generation problem - recursion with memo implementation

--- src/main/java/recursion/easy/NthFibonacci.java
+++ src/main/java/recursion/easy/NthFibonacci.java
@@ -7,4 +7,18 @@ public class NthFibonacci {
 		if (n == 2) return 1;
 		return getNthFib(n - 1) + getNthFib(n - 2);
 	}
+
+	public static int getNthFibMemo(int n) {
+		return getNthFibMemo(n, new int[n + 1]);
+	}
+
+	// O(n) time | O(n) space
+	public static int getNthFibMemo(int n, int[] memo) {
+		if (n == 1) return 0;
+		if (n == 2) return 1;
+		if (memo[n] > 0) return memo[n];
+		memo[n] = getNthFibMemo(n - 2, memo) + getNthFibMemo(n - 1, memo);
+		return memo[n];
+	}
+	
 }
--- src/test/java/recursion/easy/NthFibonacciTest.java
+++ src/test/java/recursion/easy/NthFibonacciTest.java
@@ -6,92 +6,92 @@ import static org.junit.jupiter.api.Assertions.*;
 class NthFibonacciTest {
 	@Test
 	public void TestCase1() {
-		assertTrue(NthFibonacci.getNthFib(1) == 0);
+		assertTrue(NthFibonacci.getNthFibMemo(1) == 0);
 	}
 
 	@Test
 	public void TestCase2() {
-		assertTrue(NthFibonacci.getNthFib(2) == 1);
+		assertTrue(NthFibonacci.getNthFibMemo(2) == 1);
 	}
 
 	@Test
 	public void TestCase3() {
-		assertTrue(NthFibonacci.getNthFib(3) == 1);
+		assertTrue(NthFibonacci.getNthFibMemo(3) == 1);
 	}
 
 	@Test
 	public void TestCase4() {
-		assertTrue(NthFibonacci.getNthFib(4) == 2);
+		assertTrue(NthFibonacci.getNthFibMemo(4) == 2);
 	}
 
 	@Test
 	public void TestCase5() {
-		assertTrue(NthFibonacci.getNthFib(5) == 3);
+		assertTrue(NthFibonacci.getNthFibMemo(5) == 3);
 	}
 
 	@Test
 	public void TestCase6() {
-		assertTrue(NthFibonacci.getNthFib(6) == 5);
+		assertTrue(NthFibonacci.getNthFibMemo(6) == 5);
 	}
 
 	@Test
 	public void TestCase7() {
-		assertTrue(NthFibonacci.getNthFib(7) == 8);
+		assertTrue(NthFibonacci.getNthFibMemo(7) == 8);
 	}
 
 	@Test
 	public void TestCase8() {
-		assertTrue(NthFibonacci.getNthFib(8) == 13);
+		assertTrue(NthFibonacci.getNthFibMemo(8) == 13);
 	}
 
 	@Test
 	public void TestCase9() {
-		assertTrue(NthFibonacci.getNthFib(9) == 21);
+		assertTrue(NthFibonacci.getNthFibMemo(9) == 21);
 	}
 
 	@Test
 	public void TestCase10() {
-		assertTrue(NthFibonacci.getNthFib(10) == 34);
+		assertTrue(NthFibonacci.getNthFibMemo(10) == 34);
 	}
 
 	@Test
 	public void TestCase11() {
-		assertTrue(NthFibonacci.getNthFib(11) == 55);
+		assertTrue(NthFibonacci.getNthFibMemo(11) == 55);
 	}
 
 	@Test
 	public void TestCase12() {
-		assertTrue(NthFibonacci.getNthFib(12) == 89);
+		assertTrue(NthFibonacci.getNthFibMemo(12) == 89);
 	}
 
 	@Test
 	public void TestCase13() {
-		assertTrue(NthFibonacci.getNthFib(13) == 144);
+		assertTrue(NthFibonacci.getNthFibMemo(13) == 144);
 	}
 
 	@Test
 	public void TestCase14() {
-		assertTrue(NthFibonacci.getNthFib(14) == 233);
+		assertTrue(NthFibonacci.getNthFibMemo(14) == 233);
 	}
 
 	@Test
 	public void TestCase15() {
-		assertTrue(NthFibonacci.getNthFib(15) == 377);
+		assertTrue(NthFibonacci.getNthFibMemo(15) == 377);
 	}
 
 	@Test
 	public void TestCase16() {
-		assertTrue(NthFibonacci.getNthFib(16) == 610);
+		assertTrue(NthFibonacci.getNthFibMemo(16) == 610);
 	}
 
 	@Test
 	public void TestCase17() {
-		assertTrue(NthFibonacci.getNthFib(17) == 987);
+		assertTrue(NthFibonacci.getNthFibMemo(17) == 987);
 	}
 
 	@Test
 	public void TestCase18() {
-		assertTrue(NthFibonacci.getNthFib(18) == 1597);
+		assertTrue(NthFibonacci.getNthFibMemo(18) == 1597);
 	}
 
 }
