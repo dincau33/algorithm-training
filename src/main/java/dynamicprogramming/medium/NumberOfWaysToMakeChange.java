2c5c78c096e33de3ffa77a679dedf01246f66031
dincau33 <dincau33@gmail.com>
2019-10-18 16:28:50 -0700

    Number Of Ways To Make Change problem

--- src/main/java/dynamicprogramming/medium/NumberOfWaysToMakeChange.java
+++ src/main/java/dynamicprogramming/medium/NumberOfWaysToMakeChange.java
@@ -5,15 +5,16 @@ public class NumberOfWaysToMakeChange {
 		return numberOfWaysToMakeChange(n, denoms, 0);
 	}
 
-	private static int numberOfWaysToMakeChange(int n, int[] denoms, int coinIdx) {
+	// O(n * d) | O(d) => d number of denonms; n sum
+	private static int numberOfWaysToMakeChange(int n, int[] denoms, int d) {
 		if (n == 0) return 1;
 		int ways = 0;
-		if (coinIdx < denoms.length) {
-			ways += numberOfWaysToMakeChange(n, denoms, coinIdx + 1);
+		if (d < denoms.length) {
+			ways += numberOfWaysToMakeChange(n, denoms, d + 1);
 			while (n > 0) {
-				n -= denoms[coinIdx];
+				n -= denoms[d];
 				if (n == 0) ways++;
-				else if (n > 0) ways += numberOfWaysToMakeChange(n, denoms, coinIdx + 1);
+				else if (n > 0) ways += numberOfWaysToMakeChange(n, denoms, d + 1);
 			}
 		}
 		return ways;
