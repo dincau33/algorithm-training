12f6de9a8b48d8c7015719422734d5c674bf2023
dincau33 <dincau33@gmail.com>
2019-10-18 23:19:26 -0700

    Min Number Of Ways To Make Change problem

--- src/main/java/dynamicprogramming/medium/MinNumberOfWaysToMakeChange.java
+++ src/main/java/dynamicprogramming/medium/MinNumberOfWaysToMakeChange.java
@@ -1,6 +1,7 @@
 package dynamicprogramming.medium;
 
 public class MinNumberOfWaysToMakeChange {
+	// O(n*d) time | O(n) : d is number of denominations and n is the target amount of money
 	public static int minNumberOfCoinsForChange(int n, int[] denoms) {
 		int[] smallest = new int[n + 1];
 		for (int i = 1; i < smallest.length; i++) smallest[i] = Integer.MAX_VALUE;
