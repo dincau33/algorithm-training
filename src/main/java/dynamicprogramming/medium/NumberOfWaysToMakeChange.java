133dc6f497ac3c9eb257ebe25bcdf6528d208eea
dincau33 <dincau33@gmail.com>
2019-10-24 16:40:52 -0700

    Number Of Ways To Make Change

--- src/main/java/dynamicprogramming/medium/NumberOfWaysToMakeChange.java
+++ src/main/java/dynamicprogramming/medium/NumberOfWaysToMakeChange.java
@@ -1,5 +1,9 @@
 package dynamicprogramming.medium;
 
+/*
+ * Given an array of positive integers representing coin denominations and a single non-negative integer representing a target amount of money, implement a function that returns the number of ways to make change for that target amount using the give coin denominations.
+ * Note that an unlimited amount of coins is at your disposal.
+ */
 public class NumberOfWaysToMakeChange {
 	public static int numberOfWaysToMakeChange(int n, int[] denoms) {
 		return numberOfWaysToMakeChange(n, denoms, 0);
