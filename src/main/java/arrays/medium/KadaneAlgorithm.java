ac67a4e9668856daf2089a53dce652d4ebb426fc
dincau33 <dincau33@gmail.com>
2019-10-24 16:48:35 -0700

    Kadane's Algorithm

--- src/main/java/arrays/medium/KadaneAlgorithm.java
+++ src/main/java/arrays/medium/KadaneAlgorithm.java
@@ -1,5 +1,9 @@
 package arrays.medium;
 
+/*
+ * Write a function that takes in a non-empty array of integers and returns the maximum sum that can be obtained by summing up all the numbers in a non-empty subarray of the input array.
+ * A subarray must only contain adjacent numbers.
+ */
 public class KadaneAlgorithm {
 	// O(n) time | O(1)
 	public static int kadanesAlgorithm(int[] array) {
