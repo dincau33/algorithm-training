c474a66e9a89d622ebdf0884b079386dab7c448d
dincau33 <dincau33@gmail.com>
2019-10-23 17:50:21 -0700

    Nth Fibonacci

--- src/main/java/recursion/easy/NthFibonacci.java
+++ src/main/java/recursion/easy/NthFibonacci.java
@@ -1,5 +1,8 @@
 package recursion.easy;
 
+/*
+ * The Fibonacci sequence is defined as follows: the first number of the sequence if 0, the second number is 1, and the nth number is the sum of the (n - 1)th and (n - 2)th numbers. Write a function that takes in an integer n and returns the nth Fibonacci number.
+ */
 public class NthFibonacci {
 	// O(2^n) time | O(n) space
 	public static int getNthFib(int n) {
