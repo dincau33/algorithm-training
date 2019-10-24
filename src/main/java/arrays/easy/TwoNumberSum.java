3f9cba240462cf5d8a6168896dc16ab477763643
Benoit D'Incau <dincau33@benoits-imac.lan>
2019-10-23 17:10:04 -0700

    Two Number Sum

--- src/main/java/arrays/easy/TwoNumberSum.java
+++ src/main/java/arrays/easy/TwoNumberSum.java
@@ -3,6 +3,13 @@ package arrays.easy;
 import java.util.HashSet;
 import java.util.Set;
 
+/*
+ * Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum.
+ * If any two numbers in the input array sump up to the target sum, the function should return them in an array, in sorted order.
+ * If no two numbers sum up to the target sum, the function should return an empty array.
+ * Assume that there will be at most one pair of numbers summing up to the target sum.
+ */
+
 public class TwoNumberSum {
 	// Average: O(n) time | O(n) space
 	public static int[] twoNumberSum(int[] array, int targetSum) {
