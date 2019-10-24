475d41b5abcfd37f1fc32bab7e34fc45ac296713
dincau33 <dincau33@gmail.com>
2019-10-24 15:25:23 -0700

    Smallest Difference

--- src/main/java/arrays/medium/SmallestDifference.java
+++ src/main/java/arrays/medium/SmallestDifference.java
@@ -2,6 +2,11 @@ package arrays.medium;
 
 import java.util.Arrays;
 
+/*
+ * Write a function that takes in two non-empty arrays of integers. The function should find the pair of numbers (one from the first array, one from the second array) whose absolute difference is closest to zero.
+ * The function should return an array containing these two numbers, with the number from the first array in the first position.
+ * Assume that there will only be one pair of numbers with the smallest difference.
+ */
 public class SmallestDifference {
 	public static int[] smallestDifference(int[] arrayOne, int[] arrayTwo) {
 		// 0 - sorted arrays, : O(n*log(n)) | O(1)
