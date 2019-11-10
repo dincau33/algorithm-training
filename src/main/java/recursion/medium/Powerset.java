17cb61b56647bd1d49f16a4db99652ce19148164
dincau33 <dincau33@gmail.com>
2019-11-09 16:52:51 -0800

    Balanced Brackets

--- src/main/java/recursion/medium/Powerset.java
+++ src/main/java/recursion/medium/Powerset.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
  * Write a function that takes in an array of unique integers and returns its powerset. The powerser P(X) of a set X is the set of all subsets of X. For example, the powerset of [1, 2] is [[],[1],[2],[1,2]]. Note that the sets in the powerset do not need to be in any particular order.
  */
 public class Powerset {
+    // O(n * 2 ^ n) time | O(n * 2 ^ n) space
     public static ArrayList<ArrayList<Integer>> powerset(ArrayList<Integer> array) {
         return powerset(array, array.size() - 1);
     }
--- src/main/java/stack/medium/BalancedBrackets.java
+++ src/main/java/stack/medium/BalancedBrackets.java
@@ -7,6 +7,7 @@ import java.util.Stack;
  * Write a function that takes in a string made up of brackets ("(","[","{",")","],"}") and other optional characters. The function should return a boolean representing whether or not the string isa balanced in regards to brackets. A string is said to be balanced if it has many opening brackets of a given type as it has closing brackets of that type and if no bracket is unmatched. Not that a closing bracket cannot match a corresponding opening bracket that comes after it. Similarly, brackets cannot overlap each other as in "[(])".
  */
 public class BalancedBrackets {
+    // O(n) time | O(n) space
     public static boolean balancedBrackets(String str) {
         HashMap<Character, Character> matchingBrackets = new HashMap<>();
         matchingBrackets.put(')', '(');
