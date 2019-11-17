8d6757f21542f6173fb1ef43d3c57ac3f956bee1
dincau33 <dincau33@gmail.com>
2019-11-16 18:01:36 -0800

    Min Max Stack Construction

--- src/main/java/stack/medium/MinMaxStackConstruction.java
+++ src/main/java/stack/medium/MinMaxStackConstruction.java
@@ -17,31 +17,27 @@ public class MinMaxStackConstruction {
     static class MinMaxStack {
         Stack<Integer> stack = new Stack<>();
         ArrayList<HashMap<String, Integer>> minMaxStack = new ArrayList<>();
-        int index = -1;
 
         public Integer peek() {
-            if (index < 0) throw new NoSuchElementException();
+            if (stack.size() <= 0) throw new NoSuchElementException();
             return stack.peek();
         }
 
         public Integer pop() {
-            if (index < 0) throw new NoSuchElementException();
-            minMaxStack.remove(index--);
+            if (stack.size() <= 0) throw new NoSuchElementException();
+            minMaxStack.remove(stack.size() - 1);
             return stack.pop();
         }
 
         public void push(Integer number) {
-            int currentMin = Integer.MAX_VALUE;
-            int currentMax = Integer.MIN_VALUE;
-            if (index >= 0) {
-                currentMin = getMin();
-                currentMax = getMax();
+            int currentMin = number;
+            int currentMax = number;
+            if (stack.size() > 0) {
+                currentMin = Math.min(getMin(), currentMin);
+                currentMax = Math.max(getMax(), currentMax);
             }
-            if (number < currentMin) currentMin = number;
-            if (number > currentMax) currentMax = number;
 
             stack.push(number);
-            index++;
 
             HashMap<String, Integer> currentMinMax = new HashMap<>();
             currentMinMax.put("min", currentMin);
@@ -50,13 +46,13 @@ public class MinMaxStackConstruction {
         }
 
         public Integer getMin() {
-            if (index < 0) throw new NoSuchElementException();
-            return minMaxStack.get(index).get("min");
+            if (stack.size() <= 0) throw new NoSuchElementException();
+            return minMaxStack.get(stack.size() - 1).get("min");
         }
 
         public Integer getMax() {
-            if (index < 0) throw new NoSuchElementException();
-            return minMaxStack.get(index).get("max");
+            if (stack.size() <= 0) throw new NoSuchElementException();
+            return minMaxStack.get(stack.size() - 1).get("max");
         }
     }
 }
