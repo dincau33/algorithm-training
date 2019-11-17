8a6a6b67f24e55a0a862e5d02ee65bf0b2f8233b
dincau33 <dincau33@gmail.com>
2019-11-16 16:54:10 -0800

    Search In Sorted Matrix

--- src/main/java/search/medium/SearchInSortedMatrix.java
+++ src/main/java/search/medium/SearchInSortedMatrix.java
@@ -6,7 +6,20 @@ package search.medium;
  * You are also given a target number, and you must write a function that return and array of the row and column indices of the target number if it is contained in matrix and [-1, -1] if it is not contained in the matrix.
  */
 public class SearchInSortedMatrix {
+    // Complexity: O(n + m) time | O(1) space
     public static int[] searchInSortedMatrix(int[][] matrix, int target) {
-        return null;
+        int[] targetIndices = new int[]{-1, -1};
+        int row = 0;
+        int col = matrix[row].length - 1;
+
+        while (col >= 0 && row < matrix.length) {
+            if (matrix[row][col] == target) {
+                return new int[]{row, col};
+            }
+            else if (matrix[row][col] < target) row++;
+            else col--;
+        }
+
+        return targetIndices;
     }
 }
