ee9d59c1be7d113c6a8d5f14ba3f87cf58fa22b9
dincau33 <dincau33@gmail.com>
2019-12-22 17:25:09 -0800

    Group Anagrams

--- src/main/java/strings/medium/GroupAnagrams.java
+++ src/main/java/strings/medium/GroupAnagrams.java
@@ -2,8 +2,15 @@ package strings.medium;
 
 import java.util.List;
 
+/*
+ * Write a function that takes in an array of strings and returns a list of groups of anagrams.
+ * Anagrams are strings made up of exactly the same letters, where order doesn't matter.
+ * For example, "cinema" and "iceman" are anagrams; similarly, "foo" and "ofo" are anagrams.
+ * Note that the groups aof anagrams don't need to be ordered in any particular way.
+ */
 public class GroupAnagrams {
 
+    // Complexity: O(w * n * log(n)) time | O(w * n) space - where w is the number of words and n is the length of the longest word
     public static List<List<String>> groupAnagrams(List<String> words) {
         // Write your code here.
         return null;
