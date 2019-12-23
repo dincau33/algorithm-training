4d9ce87f795af3009c8adfba61f39b427713b38b
dincau33 <dincau33@gmail.com>
2019-12-22 17:51:25 -0800

    Group Anagrams

--- src/main/java/strings/medium/GroupAnagrams.java
+++ src/main/java/strings/medium/GroupAnagrams.java
@@ -1,6 +1,6 @@
 package strings.medium;
 
-import java.util.List;
+import java.util.*;
 
 /*
  * Write a function that takes in an array of strings and returns a list of groups of anagrams.
@@ -9,10 +9,29 @@ import java.util.List;
  * Note that the groups aof anagrams don't need to be ordered in any particular way.
  */
 public class GroupAnagrams {
-
     // Complexity: O(w * n * log(n)) time | O(w * n) space - where w is the number of words and n is the length of the longest word
     public static List<List<String>> groupAnagrams(List<String> words) {
-        // Write your code here.
-        return null;
+        HashMap<String, List<String>> anagramsList = new HashMap<>();
+        for (String word:words) {
+            String sortedStr = sortString(word);
+            if (anagramsList.containsKey(sortedStr)) anagramsList.get(sortedStr).add(word);
+            else {
+                List<String> anagram = new ArrayList<>();
+                anagram.add(word);
+                anagramsList.put(sortedStr, anagram);
+            }
+        }
+        return convertMapToString(anagramsList);
+    }
+
+    public static String sortString(String str) {
+        char[] tmp = str.toCharArray();
+        Arrays.sort(tmp);
+        return new String(tmp);
+    }
+
+    public static List<List<String>> convertMapToString(Map<String, List<String>> map) {
+        Collection<List<String>> values = map.values();
+        return new ArrayList<List<String>>(values);
     }
 }
