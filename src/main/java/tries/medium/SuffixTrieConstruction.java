2ed95854fc743e0b5768f82be251c516a37b8eec
dincau33 <dincau33@gmail.com>
2019-12-25 13:43:25 -0800

    Suffix Trie Construction

--- src/main/java/tries/medium/SuffixTrieConstruction.java
+++ src/main/java/tries/medium/SuffixTrieConstruction.java
@@ -2,6 +2,13 @@ package tries.medium;
 
 import java.util.HashMap;
 
+/*
+ * Write a class for a suffix-trie-like data structure.
+ * The class should have a "root" property set to be the root node of the trie.
+ * The class should support creation from a string and the searching of strings.
+ * The creation method (called populateSuffixTrieFrom()) will be called when the class is instantiated and should populate the "root" property of the class.
+ * Note that every string added to the trie should end with the special "endSymbol" character. "*".
+ */
 public class SuffixTrieConstruction {
     static class TrieNode {
         HashMap<Character, TrieNode> children = new HashMap<Character, TrieNode>();
