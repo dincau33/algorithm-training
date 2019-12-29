e0007cfbea3c538928aea567bd3f4c977e73a04f
dincau33 <dincau33@gmail.com>
2019-12-28 23:26:19 -0800

    Four Number Sum

--- src/main/java/arrays/easy/TwoNumberSum.java
+++ src/main/java/arrays/easy/TwoNumberSum.java
@@ -13,30 +13,20 @@ import java.util.Set;
 public class TwoNumberSum {
     // Average: O(n) time | O(n) space
     public static int[] twoNumberSum(int[] array, int targetSum) {
-
         int[] numbers = {};
-        Set<Integer> set = convertToSet(array);
+        Set<Integer> set = new HashSet<>();
 
-        // for each number check if targetSum-nb is in set
         for (int first : array) {
             int second = targetSum - first;
-            if (set.contains(second) && first != second) {
+            if (set.contains(second)) {
                 numbers = first <= second ?
                         new int[]{first, second} :
                         new int[]{second, first};
+                set.remove(second);
                 break;
-            }
+            } else set.add(first);
         }
 
         return numbers;
     }
-
-    private static Set<Integer> convertToSet(int[] array) {
-        Set<Integer> set = new HashSet<>();
-        for (int n : array) {
-            set.add(n);
-        }
-        return set;
-    }
-
 }
--- src/main/java/arrays/hard/FourNumberSum.java
+++ src/main/java/arrays/hard/FourNumberSum.java
@@ -1,6 +1,6 @@
 package arrays.hard;
 
-import java.util.List;
+import java.util.*;
 
 /*
  * Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum.
@@ -8,7 +8,31 @@ import java.util.List;
  * If no four numbers sum up to the target sum, the function should return an empty array.
  */
 public class FourNumberSum {
+    // Complexity: O(n^2) time | O(n^2) space
     public static List<Integer[]> fourNumberSum(int[] array, int targetSum) {
-        return null;
+        Map<Integer, List<Integer[]>> allPairsSum = new HashMap<>();
+        List<Integer[]> quadruplets = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                int currentSum = array[i] + array[j];
+                int difference = targetSum - currentSum;
+                if (allPairsSum.containsKey(difference)) {
+                    for (Integer[] pair : allPairsSum.get(difference)) {
+                        Integer[] quadruplet = {pair[0], pair[1], array[i], array[j]};
+                        quadruplets.add(quadruplet);
+                    }
+                }
+            }
+            for (int k = 0; k < i; k++) {
+                int currentSum = array[i] + array[k];
+                Integer[] pair = {array[i], array[k]};
+                if(!allPairsSum.containsKey(currentSum)) {
+                    List<Integer[]> pairGroup = new ArrayList<Integer[]>();
+                    pairGroup.add(pair);
+                    allPairsSum.put(currentSum, pairGroup);
+                } else allPairsSum.get(currentSum).add(pair);
+            }
+        }
+        return quadruplets;
     }
 }
