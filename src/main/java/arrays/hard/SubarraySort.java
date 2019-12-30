1cbc3cf67f7df65a5e4acc93405a8ca446daf2c3
dincau33 <dincau33@gmail.com>
2019-12-29 22:46:14 -0800

    Subarray Sort

--- src/main/java/arrays/hard/SubarraySort.java
+++ src/main/java/arrays/hard/SubarraySort.java
@@ -7,6 +7,25 @@ package arrays.hard;
  */
 public class SubarraySort {
 	public static int[] subarraySort(int[] array) {
-		return null;
+		int i = 0;
+		int j = array.length - 1;
+		// Check whether the array is sorted
+		while (i < array.length -1 && array[i] <= array[i + 1]) i++;
+		while (j > 0 && array[j - 1] <= array[j]) j--;
+
+		if (i > j) return new int[]{-1, -1};
+
+		int left = i;
+		int right = j;
+		int smallest = array[i + 1];
+		int largest = array[j - 1];
+		// Find smallest and largest number out of place
+		while (i < array.length) smallest = Math.min(smallest, array[i++]);
+		while (j >= 0) largest = Math.max(largest, array[j--]);
+		// Find where smallest an largest number should be positioned
+		while (left > 0 && array[left - 1] > smallest) left--;
+		while (right < array.length - 1 && array[right + 1] < largest) right++;
+
+		return new int[]{left, right};
 	}
 }
